669515642d50ff4eb077113e7e31f8e727f5a538
MARIA <mariadaguiarocha1992@gmail.com>
2021-04-13 22:50:26 -0300

    Funcao de aleatoriedade dos pontos

--- cobra.py
+++ cobra.py
@@ -2,4 +2,11 @@
 # Jogo da cobra
 # Comandos: setas ou w-a-s-z
 
+
 import pygame, random, sys
+
+# Funcao de aleatoriedade dos pontos
+def on_grid_random():
+    x = random.randint(0,59)
+    y = random.randint(0,59)
+    return (x * 10, y * 10)
